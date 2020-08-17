8be49bf8a464ee9d187afd2b7fefce4fbe3f6a8d
clairelee <smile69414@gmail.com>
2020-08-17 15:00:02 +0800

    export and export default

---
+++ Lab2_babel_modern_js_syntax/src/lab3_course.js
@@ -0,0 +1,8 @@
+const course = {
+    name:"react with Spring Boot",
+    duration:56,
+    instructor:"Mark",
+    period:['Mondy','Turesday']
+}
+
+export default course
---
+++ Lab2_babel_modern_js_syntax/src/lab3_main1.js
@@ -0,0 +1,6 @@
+import course from './lab3_course'
+import { default_header, MAX_SIZE } from './lab3_common_util'
+
+console.log(course)
+default_header()
+console.log(MAX_SIZE)
