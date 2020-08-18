a093a3dd4beae398e4728445805a50a437c0e00f
clairelee <smile69414@gmail.com>
2020-08-18 11:15:29 +0800

    props

--- lab3_react_start/src/App.js
+++ lab3_react_start/src/App.js
@@ -4,14 +4,18 @@ import './App.css';
 import Dashboard1 from './components/Dashboard1';
 import Dashboard2 from './components/Dashboard2';
 import Person from './components/Person';
+import Pet from './components/Pet';
 
 function App() {
   return (
     <div className="App">
     <Dashboard1/>
-    <Person/>
     <Dashboard2/>
-    <Person/>
+    <Person name = "Hao" age = "43"/>
+    <Pet name = "king" species="cat"/>
+    <Person name = "Tim" age = "33">Team leader</Person>
+    <Person name = "Claire" age = "40"/>
+    <Pet/>
     </div>
   );
 }
--- lab3_react_start/src/components/Person.js
+++ lab3_react_start/src/components/Person.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
- const Person=() =>{
-    return             <p>I am groot</p>
+ const Person=props =>{
+    return ( 
+    <div>
+    <p>I am {props.name}</p>
+    <p>I am {props.age}, {Math.floor(Math.random() *5)}</p>
+    <p style={{color:"red"}}>My job is {props.children}</p>
+    </div>
+    )
 }
 
 export default Person
