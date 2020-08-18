e16ee30f006de24be2f4767d7f16565463526b70
clairelee <smile69414@gmail.com>
2020-08-18 14:09:11 +0800

    convert App.js to class component add state in App

--- lab3_react_start/src/App.js
+++ lab3_react_start/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import './App.css';
-import Dashboard1 from './components/Dashboard1';
-import Dashboard2 from './components/Dashboard2';
-import Person from './components/Person';
-import Pet from './components/Pet';
-import Counter from './components/Counter';
+import "./App.css";
+import Dashboard1 from "./components/Dashboard1";
+import Dashboard2 from "./components/Dashboard2";
+import Person from "./components/Person";
+import Pet from "./components/Pet";
+import Counter from "./components/Counter";
 
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Hao", age: 43 },
+      { name: "Tim", age: 33 },
+      { name: "Claire", age: 40 },
+    ],
+  };
 
-function App() {
-  return (
-    <div className="App">
-    <Counter step="2"/>
-    <Dashboard1/>
-    <Dashboard2/>
-    <Person name = "Hao" age = "43"/>
-    <Pet name = "king" species="cat"/>
-    <Person name = "Tim" age = "33">Team leader</Person>
-    <Person name = "Claire" age = "40"/>
-    <Pet/>
-    </div>
-  );
+  changeNameHandler = () => {
+    console.log("change button clicked");
+    this.setState({
+      persons: [
+        { name: "super man", age: 45 },
+        { name: "Caption America", age: 35 },
+        { name: "Thor", age: 200 },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Counter step="2" />
+        <Dashboard1 />
+        <Dashboard2 />
+        <button onClick={this.changeNameHandler}>change</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Pet name="king" species="cat" />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          Team leader
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+        <Pet />
+      </div>
+    );
+  }
 }
 
 export default App;
