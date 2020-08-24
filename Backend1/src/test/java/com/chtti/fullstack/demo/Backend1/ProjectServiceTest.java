6936fbe6a8f18d8c9e039ca27f5087aede18a9fb
clairelee <smile69414@gmail.com>
2020-08-24 15:39:08 +0800

    Implement add project to server

--- Backend1/src/test/java/com/chtti/fullstack/demo/Backend1/ProjectServiceTest.java
+++ Backend1/src/test/java/com/chtti/fullstack/demo/Backend1/ProjectServiceTest.java
@@ -4,13 +4,14 @@ import com.chtti.fullstack.demo.Backend1.model.Project;
 import com.chtti.fullstack.demo.Backend1.service.ProjectService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class ProjectServiceTest {
-//    @Autowired
+    @Autowired
     private ProjectService service;
 
     @BeforeEach
