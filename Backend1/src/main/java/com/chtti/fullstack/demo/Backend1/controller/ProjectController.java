e0737384be5ea7ed9cec2dfeba01a2289c2cc9c1
clairelee <smile69414@gmail.com>
2020-08-25 10:32:18 +0800

    exception handler for internal 500 to HTTP 400

--- Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/controller/ProjectController.java
+++ Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/controller/ProjectController.java
@@ -2,22 +2,35 @@ package com.chtti.fullstack.demo.Backend1.controller;
 
 import com.chtti.fullstack.demo.Backend1.model.Project;
 import com.chtti.fullstack.demo.Backend1.service.ProjectService;
+import com.chtti.fullstack.demo.Backend1.utility.MapValidationError;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
     @Autowired
     ProjectService projectService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProjectController.class);
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?>
+    createNewProject(@Valid @RequestBody Project project,
+                     BindingResult bindingResult) {
+        LOGGER.info("get project from json={}", project);
+        ResponseEntity<Map<String, String>> errorMap = MapValidationError.MapValidation(bindingResult);
+        if (errorMap != null) return errorMap;
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project1, HttpStatus.CREATED);
     }
--- Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/service/ProjectService.java
+++ Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/service/ProjectService.java
@@ -1,5 +1,6 @@
 package com.chtti.fullstack.demo.Backend1.service;
 
+import com.chtti.fullstack.demo.Backend1.exceptions.ProjectIdException;
 import com.chtti.fullstack.demo.Backend1.model.Project;
 import com.chtti.fullstack.demo.Backend1.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,15 @@ import org.springframework.stereotype.Service;
 public class ProjectService {
     @Autowired
     private ProjectRepository repository;
-    public Project saveOrUpdateProject(Project project){
-        return repository.save(project);
+
+    public Project saveOrUpdateProject(Project project) {
+        String upperCaseProjectId = project.getProjectIdentifier().toUpperCase();
+        try {
+            project.setProjectIdentifier(upperCaseProjectId);
+            return repository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException(String.format("Project ID %s already exists",
+                    upperCaseProjectId));
+        }
     }
 }
